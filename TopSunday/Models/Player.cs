b7f115ec4b2f40085ddcbdc5d4d17c47cf328192
David Oliveira <david.tavares.oliveira@everis.com>
2015-08-19 03:50:13 +0100

    .

---
+++ TopSunday/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TopSunday.Models
+{
+    public class Player
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Stars { get; set; }
+        public int PlayedGames { get; set; }
+        public int Wins { get; set; }
+        public int Looses { get; set; }
+        public bool Win { get; set; }
+    }
+}
