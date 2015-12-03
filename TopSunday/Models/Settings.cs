0260a0cb0dc34e13f3683381845912a375cd2df0
david.tavares.oliveira@gmail.com <david.tavares.oliveira@gmail.com>
2015-12-03 03:01:57 +0000

    add settings class

---
+++ TopSunday/Models/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TopSunday.Models
+{
+    public class Settings
+    {
+        [Key]
+        public int ID { get; set; }
+        public int TablePosition { get; set; }
+
+        [ForeignKey("GameDay")]
+        public int GameTypeID { get; set; }
+        public virtual GameDay GameDay { get; set; }
+    }
+}
