c279b70aed8d4fb0d90d83a8defd17409cb19881
david.tavares.oliveira@gmail.com <david.tavares.oliveira@gmail.com>
2015-12-09 01:07:51 +0000

    aa

---
+++ TopSunday/Controllers/SundayViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TopSunday.Controllers
+{
+    public class SundayViewModel
+    {
+        public List<Models.Classification> Classification { get; set; }
+    }
+}
